bb2da6bbb67e40b5e42d97b50629a1a3f69fe171
Ajaydodiya <139394815+Ajaydodiya7773@users.noreply.github.com>
2024-01-29 22:37:51 +0530

    29/01/24 work

--- src/componenet/Intro/intro.js
+++ src/componenet/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./intro.css";
-import bg from "../Intro/BG.png";
+import bg from "../../assets/bg2.png";
 import { Link } from "react-scroll";
 import btnImg from '../../assets/hireme.png';
 const Intro = () => {
@@ -11,24 +11,21 @@ const Intro = () => {
         <span className="introText">
           I'm <span className="introName">Ajay Singh</span>
           <br />
-          Frontend Developer.
+          Web & Python Developer.
         </span>
         <p className="introPara">
-          Certainly! If you prefer a more concise and catchy headline
-          emphasizing your enthusiasm for technology, you can go with: Headline:
-          "Tech Enthusiast | MCA Student | Frontend Developer | MERN Stack |
-          JavaScript | Python" Bio: Passionate about all things tech! 🚀 MCA
+      
+          Tech Enthusiast Passionate about all things tech! 🚀 MCA PostGraduate
           student diving deep into the world of Frontend Development.
           Specializing in the dynamic MERN stack, mastering JavaScript, Python,
           and always up for a coding challenge. Eager to contribute innovative
           solutions and connect with like-minded tech enthusiasts. Let's geek
-          out over the latest trends and opportunities! 💡💻 #TechEnthusiast
-          #MERNStack #FrontendDev
+          out over the latest trends and opportunities!💡💻
         </p>
-        <Link> <button className="btn"/><img src={btnImg} alt="Hire Me"/></Link>
+        <Link> <button className="btn" ><img src={btnImg} alt="Hire Me" className="btnImg"/>Hire me</button></Link>
 
       </div>
-      <span>  <img src={bg} alt="Profile" className="bg" /></span>
+      <img src={bg} alt="Profile" className="bg" />
     
     </section>
   );
